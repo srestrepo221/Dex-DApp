18c2e39cd4f86999967d068992c974aa882131cc
Santiago restrepo <santiagorestrepo@santiagos-mbp-3.attlocal.net>
2022-08-17 17:01:43 -0400

    18. Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import {
   loadExchange
 } from '../store/interactions';
 
+import Navbar from './Navbar'
+
 function App() {
   const dispatch = useDispatch()
 
@@ -21,8 +23,16 @@ function App() {
     // Fetch current networks's chainId(e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-     await loadAccount(provider, dispatch)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch)
+    })
+   
 
    
 
@@ -43,7 +53,7 @@ useEffect(() => {
   return (
     <div>
 
-      {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
