9aa4ab4e44baafb6de7cecd831fc20445f3bfe46
Santiago restrepo <santiagorestrepo@santiagos-mbp-3.attlocal.net>
2022-09-27 22:03:51 -0400

    30. Fill orders

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -57,6 +57,11 @@ export const subscribeToEvents = (exchange, dispatch) => {
     dispatch({ type: 'ORDER_CANCEL_SUCCESS', order, event })
   })
 
+    exchange.on('Trade', (id, user, tokenGet, amountGet, tokenGive, amountGive, creator, timestamp, event) => {
+    const order = event.args
+    dispatch({ type: 'ORDER_FILL_SUCCESS', order, event })
+  })
+
   exchange.on('Deposit', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
@@ -69,6 +74,7 @@ export const subscribeToEvents = (exchange, dispatch) => {
     const order = event.args
     dispatch({ type: 'NEW_ORDER_SUCCESS', order, event })
   })
+
 }
 
 // ------------------------------------------------------------------------------
@@ -180,15 +186,12 @@ export const makeSellOrder = async (provider, exchange, tokens, order, dispatch)
   }
 }
 
-
-
 // ------------------------------------------------------------------------------
 // CANCEL ORDER
 
 export const cancelOrder = async (provider, exchange, order, dispatch) => {
-
   dispatch({ type: 'ORDER_CANCEL_REQUEST' })
-
+  
   try {
     const signer = await provider.getSigner()
     const transaction = await exchange.connect(signer).cancelOrder(order.id)
@@ -197,3 +200,25 @@ export const cancelOrder = async (provider, exchange, order, dispatch) => {
     dispatch({ type: 'ORDER_CANCEL_FAIL' })
   }
 }
+
+
+// ------------------------------------------------------------------------------
+// FILL ORDER
+
+export const fillOrder = async (provider, exchange, order, dispatch) => {
+  dispatch({ type: 'ORDER_FILL_REQUEST' })
+  
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).fillOrder(order.id)
+    await transaction.wait()
+  } catch (error) {
+    dispatch({ type: 'ORDER_FILL_FAIL' })
+  }
+}
+
+
+
+
+
+
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -73,7 +73,13 @@ const DEFAULT_EXCHANGE_STATE = {
     isSuccessful: false
   },
   allOrders: {
-    loaded: false,
+  	loaded: false,
+    data: []
+  },
+  cancelledOrders: {
+    data: []
+  },
+  filledOrders: {
     data: []
   },
   events: []
@@ -158,8 +164,54 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
     		isSuccessful: false,
     		isError: true
     		}
-    	}	
+    	}
+  	
+  	// ------------------------------------------------------------------------------
+    // FILLING ORDERS
+    case 'ORDER_FILL_REQUEST':
+    return {
+    	...state,
+    	transaction: {
+    		transactionType: "Fill Order",
+    		isPending: true,
+    		isSuccessful: false
+    	}
+    }
 
+    case 'ORDER_FILL_SUCCESS':
+    // Prevent duplicate orders
+    index = state.filledOrders.data.findIndex(order => order.id.toString() === action.order.id.toString())
+
+	if(index === -1) {
+		data = [...state.filledOrders.data, action.order]
+	} else {
+		data = state.filledOrders.data
+	}
+
+	return {
+		...state,
+		transaction: {
+			transactionType: "Fill Order",
+			isPending: false,
+			isSuccessful: true
+		},
+		filledOrders: {
+			...state.filledOrders,
+			data
+		},
+		events: [action.event, ...state.events]
+	}
+
+	case 'ORDER_FILL_FAIL':
+	return {
+		...state,
+		transaction: {
+			transactionType: "Fill Order",
+			isPending: false,
+			isSuccessful: false,
+			isError: true
+		}
+	}
     // ------------------------------------------------------------------------------
     // BALANCE CASES
     case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
